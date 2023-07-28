5db3e523b592aef1bf6ee2b150755ad59ea6279c
Кот Игорь Александрович <kotia@irbis.local>
2023-07-28 12:33:05 +0700

    Vite

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
+
+export default {
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist')
+  },
+  plugins: [handlebars(
+  )],
+};
